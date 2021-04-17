6ea9e15b203cecc6f05e8ca3c0d796997e5905fd
Dios Vo <vtmn1212@gmail.com>
2021-04-17 14:45:51 +0700

    Added anootation service into OrderService

--- SaleApp/src/main/java/com/dv/service/impl/OrderServiceImpl.java
+++ SaleApp/src/main/java/com/dv/service/impl/OrderServiceImpl.java
@@ -4,11 +4,13 @@ import com.dv.pojo.Cart;
 import com.dv.service.OrderService;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author diosvo
  */
+@Service
 public class OrderServiceImpl implements OrderService {
 
     @Autowired
